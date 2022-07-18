d4d9a9fe5f0fa5c754e3a76bf24b968516407cbf
Ryan Littlefield <segalcsam@gmail.com>
2022-07-18 14:46:56 -0600

    working real nice

--- calls.py
+++ calls.py
@@ -14,13 +14,18 @@ def getCustomer(email):
     url = "https://"+os.getenv("STORE_NAME")+"/admin/api/2022-10/customers/search.json?query="+email 
     x = requests.get(url,headers=h)
     return x.json()
-def createDraftOrder() :
-    return
+def createDraftOrder(customerid,title,depositYN,price) :
+    d = {"draft_order":{"line_items":[{"title":title+" "+depositYN,"price":str(price),"quantity":2}],"customer":{"id":customerid},"use_customer_default_address":'true'}}
+    h = {"X-Shopify-Access-Token":key, "Content-Type":"application/json"}
+    x = requests.post(URLforDraft,data=json.dumps(d),headers=h)
+    return x
 
 #x = requests.post(URLforDraft,data=json.dumps(d),headers=h)
 #print(x.status_code)
 
 if __name__ == "__main__" :
     response = getCustomer("philiptester@gmail.com")
-    print(response["customers"][0]["id"])
+    customerID = response["customers"][0]["id"]
+    res = createDraftOrder(customerID,"OLD SHIRT","DEPOSIT",36.55)
+    print(res.status_code)
 
