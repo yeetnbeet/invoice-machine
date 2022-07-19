6d1ca560d78fb2819a1ab8927272e2d3a11a159f
Ryan Littlefield <segalcsam@gmail.com>
2022-07-18 18:23:47 -0600

    almost there

--- calls.py
+++ calls.py
@@ -26,35 +26,23 @@ def getCustomer(email):
     url = "https://"+os.getenv("STORE_NAME")+"/admin/api/2022-10/customers/search.json?query="+email 
     x = requests.get(url,headers=h)
     return x.json()
+    
 def createDraftOrder(customerid,title,depositYN,price) :
     d = {"draft_order":{"line_items":[{"title":title+" "+depositYN,"price":str(price),"quantity":1}],"customer":{"id":customerid},"use_customer_default_address":'true'}}
     h = {"X-Shopify-Access-Token":key, "Content-Type":"application/json"}
     x = requests.post(URLforDraft,data=json.dumps(d),headers=h)
     return x
+
 def getOrders():    
     credentials = None
-
     service = discovery.build('sheets', 'v4', credentials=credentials)
-
-# The ID of the spreadsheet to retrieve data from.
-    spreadsheet_id = '1gr7wB9hHDgTA3tg3dfUjYkVsNoJPF7Ltwgb9VeHsp-0'  # TODO: Update placeholder value.
-
-# The A1 notation of the values to retrieve.
-    range_ = 'B2:Q7'  # TODO: Update placeholder value.
-
-# How values should be represented in the output.
-# The default render option is ValueRenderOption.FORMATTED_VALUE.
-    value_render_option = 'FORMATTED_VALUE'  # TODO: Update placeholder value.
-
-# How dates, times, and durations should be represented in the output.
-# This is ignored if value_render_option is
-# FORMATTED_VALUE.
-# The default dateTime render option is [DateTimeRenderOption.SERIAL_NUMBER].
-    date_time_render_option = 'SERIAL_NUMBER'  # TODO: Update placeholder value.
+    spreadsheet_id = '1gr7wB9hHDgTA3tg3dfUjYkVsNoJPF7Ltwgb9VeHsp-0'
+    range_ = 'B2:Q7'
+    value_render_option = 'FORMATTED_VALUE'
+    date_time_render_option = 'SERIAL_NUMBER'
 
     request = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_, valueRenderOption=value_render_option, dateTimeRenderOption=date_time_render_option)
     response = request.execute()
-    
     return response
 
 #x = requests.post(URLforDraft,data=json.dumps(d),headers=h)
@@ -67,5 +55,6 @@ if __name__ == "__main__" :
     #customerID = response["customers"][0]["id"]
     #res = createDraftOrder(customerID,itemDescription,"DEPOSIT",Price)
     #print(res.status_code)
-    getOrder()
+    print(getOrders())
+
 
